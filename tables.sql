51a07cdaf0905cc1a3f60b2b2a7d0b13059ead02
Ofentse <ofentsesambo@gmail.com>
2020-02-26 16:06:03 +0200

    second commit

---
+++ tables.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE umuzi;
+
+CREATE TABLE Customer(
+   CustomerID SERIAL PRIMARY KEY,
+   FirstName VARCHAR(50),
+   LastName VARCHAR(50),
+   Gender VARCHAR,
+   Address VARCHAR(200),
+   Phone INT,
+   Email VARCHAR(100),
+   City VARCHAR(20),
+   Country VARCHAR(50),
+   PRIMARY KEY(CustomerID)
+);
+
+CREATE TABLE Employees(
+   EmployeeID SERIAL PRIMARY KEY,
+   FirstName VARCHAR(50),
+   LastName VARCHAR(50),
+   Email VARCHAR(100),
+   JobTitle VARCHAR(20)
+);
+
+CREATE TABLE Orders(
+   OrderID INT,
+   ProductID INT,
+   PaymentID INT,
+   FullfilledByEmployeeID INT,
+   DateRequired DATE,
+   DateShipped DATE,
+   Status VARCHAR
+   
+);
+
+CREATE TABLE Payments(
+   CustomerID INT,
+   PaymentID INT,
+   PaymentDate DATE,
+   Amount DECIMAL
+   
+);
+
+CREATE TABLE Products(
+   ProductID INT,
+   ProductName VARCHAR(100),
+   Description VARCHAR(300),
+   BuyPrice DECIMAL
+   
+);
+
+INSERT INTO Customer(FirstName, LastName, Gender, Address, Phone, Email, City, Country)
+VALUES ('JOHN', 'HIBERT', 'MALE', '284 CHAUCER ST', '084789657', 'JOHN@GMAIL.COM', 'JOHANNESBURG', 'SOUTH AFRICA'),
+  ('THANDO', 'SITHOLE', 'FEMALE', '240 SECT 1', '0794445584', 'THANO@GMAIL.COM', 'CAPE TOWN', 'SOUTH AFRICA'),
+  ('LEON', 'GLEN', 'MALE', '81 EVERTON RD GILLITS', '0820832830', 'LEON@GMAIL.COM', 'DURBAN', 'SOUTH AFRICA'),
+  ('CHARL', 'MULLER', 'MALE', '290A DORSET ECKE', '44856872553', 'CARL.MULLER@YAHOO.COM', 'BERLIN', 'GERMANY'),
+  ('JULIA', 'STEIN', 'FEMALE', '2 WERNERRING', '448672445058', 'JS234@YAHOO.COM', 'FRANKFURT', 'GERMANY');
+
+INSERT INTO Employees(FirstName, LastName, Email, JobTitle)
+VALUES ('KANI', 'MATTHEW', 'MAT@GMAIL.COM', 'MANAGER'),
+   ('LESLY', 'CRONJE', 'LESC@GMAIL.COM', 'CLERK'),
+   ('GIDEON', 'MADUKU', 'M@GMAIL.COM', 'ACCOUNTANT');
+
+INSERT INTO Orders(OrderID, ProductID, PaymentID, FullfilledByEmployeeID, DateRequired, DateShipped, Status)
+VALUES ('1', '1', '1', '2', '05/09/2018',' ', 'Not Shipped'),
+  ('2', '1', '2', '2', '04/09/2018','04/09/2018', 'Shipped'),
+  ('3', '3', '3', '3', '06/09/2018',' ', 'Not Shipped');
